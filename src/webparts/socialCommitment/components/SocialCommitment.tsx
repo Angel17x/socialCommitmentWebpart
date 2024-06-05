7777a03870efd54de35a97570ebc44f4c3e5f4aa
Angel Lugo <angell@ateneamercantil.com>
2024-06-05 10:03:30 -0400

    changes

--- src/webparts/socialCommitment/components/SocialCommitment.tsx
+++ src/webparts/socialCommitment/components/SocialCommitment.tsx
@@ -1,75 +1,92 @@
 import * as React from 'react';
 import styles from './SocialCommitment.module.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import type { ISocialCommitmentProps } from './ISocialCommitmentProps';
-import { HttpClient, HttpClientResponse } from '@microsoft/sp-http';
+import { HttpClient } from '@microsoft/sp-http';
 import { MSocialCommitment } from '../models/MSocialCommitment';
+import Slider from 'react-slick';
 
-
-// Define the state interface
 interface ISocialCommitmentState {
   list: MSocialCommitment[];
+  isLoading: boolean;
+  error: string | undefined;
 }
+
 export default class SocialCommitment extends React.Component<ISocialCommitmentProps, ISocialCommitmentState> {
-
   constructor(props: ISocialCommitmentProps) {
-    super(props)
-
+    super(props);
     this.state = {
-      list: [] as MSocialCommitment[]
-    }
-}
-public render(): React.ReactElement<ISocialCommitmentProps> {
-  return (
-    <div className={styles.socialCommitmentCard}>
-      {this.state.list.map((socialCommitment:MSocialCommitment) => {
-        const absoluteUrl = this.props.context.pageContext.web.absoluteUrl; 
-        return (
-        <div key={socialCommitment.Id} >
-          <div>
-            <img src={`${absoluteUrl}${socialCommitment?.AttachmentFiles?.results![0].ServerRelativeUrl}`} alt={socialCommitment.Title} />
-          </div>
-          <div>{socialCommitment.Title}</div>
-          <div>{socialCommitment.Descripci_x00f3_n}</div>
-        </div>
-      )})}
-    </div>
-  );
-}
-
-  public componentDidMount() {
-    this.getListItems().then((items:MSocialCommitment[]) => {
-      
-      this.setState({ list: items });
-      console.log(items);// Aquí puedes establecer el estado con los items, si es necesario
-    });
+      list: [],
+      isLoading: true,
+      error: undefined
+    };
   }
 
-  private getListItems(): Promise<any> {
-  // Asegúrate de que absoluteUrl apunta al subsitio correcto
-  const absoluteUrl = this.props.context.pageContext.web.absoluteUrl; // Esto debería ser https://stevemacd.sharepoint.com/sites/WEBPRUEBAS
-  const listUrl: string = `${absoluteUrl}/sites/WEBPRUEBAS/_api/web/lists/getByTitle('Compromiso Social')/items?$select=Id,Title,Descripci_x00f3_n,AttachmentFiles&$expand=AttachmentFiles`;
+  public componentDidMount() {
+    this.getListItems();
+  }
 
-    console.log(absoluteUrl);
-    console.log(listUrl);
-  
-    return this.props.context.httpClient.get(listUrl, HttpClient.configurations.v1, {
+  private getListItems(): void {
+    const absoluteUrl = this.props.context.pageContext.web.absoluteUrl;
+    const listUrl = `${absoluteUrl}/sites/WEBPRUEBAS/_api/web/lists/getByTitle('Compromiso Social')/items?$select=Id,Title,Descripci_x00f3_n,AttachmentFiles&$expand=AttachmentFiles`;
+
+    this.props.context.httpClient.get(listUrl, HttpClient.configurations.v1, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json;odata=verbose',
-
       }
     })
-      .then((response: HttpClientResponse ) => {
-        return response.json();
-      })
-      .then((json:any) => {
-        if (json.d && json.d.results) {
-          return json.d.results as MSocialCommitment[]; // Devuelve directamente los resultados
-        } else {
-          throw new Error("No results found");
-        }
-      }).catch((error: any) => {
-        console.error('Error fetching list items', error);
-      });
+    .then(response => response.json())
+    .then(json => {
+      if (json.d && json.d.results) {
+        this.setState({ list: json.d.results, isLoading: false });
+      } else {
+        throw new Error("No results found");
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching list items', error);
+      this.setState({ error: error.toString(), isLoading: false });
+    });
+  }
+
+  public render(): React.ReactElement<ISocialCommitmentProps> {
+    const { isLoading, error, list } = this.state;
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    };
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      return <div>Error: {error}</div>;
+    }
+
+    return (
+      <div className={styles.container}>
+        <Slider {...settings}>
+          {list.map((item: MSocialCommitment) => (
+            <div key={item.Id} className={styles.paddingCard}>
+              <div className={styles.socialCommitmentCard}>
+                <div className={styles.containerImage}>
+                  <img src={`${this.props.context.pageContext.web.absoluteUrl}${item?.AttachmentFiles?.results![0].ServerRelativeUrl}`} alt={item.Title} />
+                </div>
+                <h2>{item.Title}</h2>
+                <p>{item.Descripci_x00f3_n}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
   }
 }
--- src/webparts/socialCommitment/models/MSocialCommitment.ts
+++ src/webparts/socialCommitment/models/MSocialCommitment.ts
@@ -1,36 +1,36 @@
 export interface MSocialCommitment {
-  __metadata?:        ResultMetadata;
-  AttachmentFiles?:   AttachmentFiles;
-  Id?:                number;
-  Title?:             string;
-  Descripci_x00f3_n?: string;
-  ID?:                number;
+  __metadata:        ResultMetadata;
+  AttachmentFiles:   AttachmentFiles;
+  Id:                number;
+  Title:             string;
+  Descripci_x00f3_n: string;
+  ID:                number;
 }
 
 export interface AttachmentFiles {
-  results?: Result[];
+  results: Result[];
 }
 
 export interface Result {
-  __metadata?:         ResultMetadata;
-  FileName?:           string;
-  FileNameAsPath?:     Path;
-  ServerRelativePath?: Path;
-  ServerRelativeUrl?:  string;
+  __metadata:         ResultMetadata;
+  FileName:           string;
+  FileNameAsPath:     Path;
+  ServerRelativePath: Path;
+  ServerRelativeUrl:  string;
 }
 
 export interface Path {
-  __metadata?: FileNameAsPathMetadata;
-  DecodedUrl?: string;
+  __metadata: FileNameAsPathMetadata;
+  DecodedUrl: string;
 }
 
 export interface FileNameAsPathMetadata {
-  type?: string;
+  type: string;
 }
 
 export interface ResultMetadata {
-  id?:   string;
-  uri?:  string;
-  type?: string;
-  etag?: string;
+  id:   string;
+  uri:  string;
+  type: string;
+  etag: string;
 }
